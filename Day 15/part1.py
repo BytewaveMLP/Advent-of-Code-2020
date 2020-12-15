4f63bbb46e9a26142a410b70ea34aec0a3e19f08
Eliot Partridge <byte@code.horse>
2020-12-14 23:19:07 -0600

    Complete day 15
    (naively)

---
+++ Day 15/part1.py
@@ -0,0 +1,24 @@
+import sys
+input_lines = [line for line in open(sys.argv[1]).read().split('\n') if line != '']
+numbers = [int(n) for n in input_lines[0].split(',')]
+
+LIMIT = 2020
+
+last_spoken = {}
+for i, n in enumerate(numbers):
+	last_spoken[n] = i + 1
+
+last_last_said = 0
+last_said = numbers[-1]
+
+print(last_spoken)
+
+for turn in range(len(numbers), LIMIT):
+	last_last_said = last_said
+	if last_said in last_spoken:
+		last_said = turn - last_spoken[last_said]
+	else:
+		last_said = 0
+	last_spoken[last_last_said] = turn
+
+print(last_said)
