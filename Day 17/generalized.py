5f4a57e0e7a25ec97155712affe52b8eec85245c
Eliot Partridge <byte@code.horse>
2020-12-17 16:18:54 -0600

    day 17: Bounds-check args, don't print slice for 2D

--- Day 17/generalized.py
+++ Day 17/generalized.py
@@ -5,13 +5,21 @@ import operator
 from typing import *
 
 if not (2 <= len(sys.argv) <= 4):
-	print(f'usage: {sys.argv[0]} <input.txt> [<dimensions>] [<iterations>]')
+	print(f'usage: {sys.argv[0]} <input.txt> [<dimensions>] [<iterations>]', file=sys.stderr)
 	exit(1)
 
 Point = Tuple[int, ...]
 Grid = set[Point]
 DIMENSIONS = int(sys.argv[2] if len(sys.argv) > 2 else 3)
+if DIMENSIONS < 2:
+	print('dimemsions must be >= 2', file=sys.stderr)
+	exit(1)
+
 ITERATIONS = int(sys.argv[3] if len(sys.argv) > 3 else 6)
+if ITERATIONS < 1:
+	print('iterations must be >= 1', file=sys.stderr)
+	exit(1)
+
 NEIGHBOR_OFFSETS = list(itertools.product([-1, 0, 1], repeat=DIMENSIONS))
 
 addpos = lambda x, y: tuple(map(operator.add, x, y))
@@ -29,7 +37,8 @@ def print_grid(grid: Grid):
 		# reverse dimensions iteration order to make visualization make more sense
 		# (iterate lower dimensions before higher ones)
 		higher_dims = higher_dims[::-1]
-		print(f'slice ({", ".join(str(d) for d in higher_dims)})')
+		# don't bother printing the slice if the tuple is empty (2D)
+		if higher_dims: print(f'slice ({", ".join(str(d) for d in higher_dims)})')
 		# print this dimension's x,y slice
 		for y in RANGE_DIMS[1]:
 			for x in RANGE_DIMS[0]:
